05fe9520851094eb03effce5b32131106cf68c55
mohan dandigam <mohandand@gmail.com>
2023-04-16 21:18:01 -0500

    Added base code

--- debounce-search/src/Search.js
+++ debounce-search/src/Search.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Search() {
     return (
-        <div>
-            Hellow
-            </div>
+        <div className="container">
+            <input type="text" placeholder="Type Something"></input>
+            <button>Search</button>
+        </div>
     )
 }
